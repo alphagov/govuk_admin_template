ecf07966dc5948ba730d525c101dab3a22a52058
Paul Hayes <fofr@users.noreply.github.com>
2014-05-15 15:29:33 +0100

    Add a generated dummy app
    * Generated from running `rails plugin new …`

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in govuk_admin_template.gemspec
 gemspec
+
+# Dummy application dependencies
+# gem "jquery-rails"
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '8a54d725968f21fbc6f653c8f9e3bd9bdf17e22b8173e821b27c8e66914e5b439e774818d1d0d68bf805968cfb802d381ef56511390cbd8f4ae88e223a971c06'
