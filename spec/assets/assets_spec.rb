9bda84fa6b73de5ef84af157fc8b8e9aa12080d3
Paul Hayes <fofr@users.noreply.github.com>
2014-05-15 17:24:20 +0100

    Add asset test for IE shims

--- spec/assets/assets_spec.rb
+++ spec/assets/assets_spec.rb
@@ -3,18 +3,22 @@ require 'spec_helper'
 describe 'Assets' do
   subject(:asset) { page.body }
 
-  describe 'Static' do
-    it 'provides the crown header' do
-      visit '/assets/header-crown.png'
-      asset.should_not be_empty
-    end
+  it 'serves images, eg the crown header' do
+    visit '/assets/header-crown.png'
+    asset.should_not be_empty
   end
 
-  describe 'Compiled JS' do
-    it 'includes library code' do
+  describe 'Compiling javascript' do
+    it 'compiles library code' do
       visit '/assets/govuk_admin_template.js'
       expect(asset).to include('Bootstrap')
       expect(asset).to include('jQuery')
+    end
+
+    it 'compiles IE shims' do
+      visit '/assets/lte-ie8.js'
+      expect(asset).to include('respond')
+      expect(asset).to include('html5')
     end
   end
 end
