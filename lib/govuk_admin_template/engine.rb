8a38503bc57488aca1c3b739be5ca32a2a6bcc0b
Paul Hayes <fofr@users.noreply.github.com>
2014-07-04 10:29:34 +0100

    Precompile IE7 to a CSS file, not SCSS
    * The precompile list needs to list the output filenames, not the
    source filenames.

--- lib/govuk_admin_template/engine.rb
+++ lib/govuk_admin_template/engine.rb
@@ -4,7 +4,7 @@ module GovukAdminTemplate
     require 'jquery-rails'
 
     initializer 'govuk_admin_template.assets.precompile' do |app|
-      app.config.assets.precompile += %w(lte-ie8.js govuk-admin-template.js govuk_admin_template/bootstrap-ie7.scss)
+      app.config.assets.precompile += %w(lte-ie8.js govuk-admin-template.js govuk_admin_template/bootstrap-ie7.css)
     end
 
     # User friendly GOV.UK date formats, based on:
