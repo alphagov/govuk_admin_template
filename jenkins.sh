e46fd134f110afa9d4885804e03076480d094a88
Tijmen Brommet <tijmen@gmail.com>
2015-11-23 15:01:30 +0000

    Experiment - WIP

--- jenkins.sh
+++ jenkins.sh
@@ -5,7 +5,18 @@ rm -f Gemfile.lock
 git clean -fdx
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 
-bundle exec rake
+for ruby_version in 1.9.3 2.1 2.2; do
+  for gemfile in rails_3.2 rails_4.0 rails_4.1 rails_4.2; do
+    RBENV_VERSION=${ruby_version} bundle install \
+      --path "${HOME}/bundles/${JOB_NAME}" \
+      --gemfile "gemfiles/${gemfile}.gemfile"
+
+    RBENV_VERSION=${ruby_version} \
+      BUNDLE_PATH="${HOME}/bundles/${JOB_NAME}" \
+      BUNDLE_GEMFILE="gemfiles/${gemfile}.gemfile" \
+      bundle exec rake
+  done
+done
 
 if [[ -n "$PUBLISH_GEM" ]]; then
   bundle exec rake publish_gem
