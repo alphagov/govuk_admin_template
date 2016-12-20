e5a5d16dcb57a9d3720c409039daa028c670a450
Andrew Vos <andrew.vos@gmail.com>
2016-12-20 10:38:39 +0000

    Compile correct favicon for production
    Simple typo :)
    
    Turns out #139 was incorrect.

--- lib/govuk_admin_template/engine.rb
+++ lib/govuk_admin_template/engine.rb
@@ -6,7 +6,7 @@ module GovukAdminTemplate
     initializer 'govuk_admin_template.assets.precompile' do |app|
       app.config.assets.precompile += %w(lte-ie8.js govuk-admin-template.js govuk_admin_template/bootstrap-ie7.css)
       app.config.assets.precompile += %w(govuk_admin_template/favicon-development.png govuk_admin_template/favicon-integration.png
-                                          govuk_admin_template/favicon-preview.png govuk_admin_template/favicon.production.png govuk_admin_template/favicon.png)
+                                          govuk_admin_template/favicon-preview.png govuk_admin_template/favicon-production.png govuk_admin_template/favicon.png)
 
     end
 
