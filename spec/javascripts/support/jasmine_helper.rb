038866f6fadc74164f68d41f2eb302baae411dfb
Ollie Treend <ollie.treend@digital.cabinet-office.gov.uk>
2022-02-02 11:25:10 +0000

    Run JS tests with Selenium instead of PhantomJS
    The Jasmine tests in `spec/javascripts/` were running with PhantomJS. However PhantomJS has long been unmaintained, so it's not a good idea to keep using it.
    
    Instead, this commit uses the `jasmine_selenium_runner` gem to make Jasmine run tests in Selenium. Whilst this gem has recently been [discontinued], it at least brings us in line with other GOV.UK projects which also [need a new approach][jasmine-eol-issue] to JavaScript testing.
    
    I've also included the `govuk_test` gem which configures Selenium to run tests in a headless Chrome browser – again, to be [consistent with GOV.UK projects][govuk-conventions].
    
    [discontinued]: https://github.com/jasmine/jasmine_selenium_runner#discontinued
    [jasmine-eol-issue]: https://github.com/alphagov/govuk_publishing_components/issues/2555
    [govuk-conventions]: https://docs.publishing.service.gov.uk/manual/conventions-for-rails-applications.html#use-gov-uk-supporting-gems

--- spec/javascripts/support/jasmine_helper.rb
+++ spec/javascripts/support/jasmine_helper.rb
@@ -1,9 +1,8 @@
-# Use this file to set/override Jasmine configuration options
-# You can remove it if you don't need it.
-# This file is loaded *after* jasmine.yml is interpreted.
-#
-# Example: using a different boot file.
-# Jasmine.configure do |config|
-#   config.boot_dir = '/absolute/path/to/boot_dir'
-#   config.boot_files = lambda { ['/absolute/path/to/boot_dir/file.js'] }
-# end
+require "jasmine_selenium_runner/configure_jasmine"
+require "govuk_test"
+
+class ChromeHeadlessJasmineConfigurer < JasmineSeleniumRunner::ConfigureJasmine
+  def selenium_options
+    { options: GovukTest.headless_chrome_selenium_options }
+  end
+end
